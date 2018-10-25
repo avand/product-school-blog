1fba3809a7d58aa4f43ad720fe812dad92ae55ef
Avand Amiri <avand@avandmamiri.com>
2018-10-25 02:44:03 +0000

    Login via email no password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,30 @@ class SessionsController < ApplicationController
     end
     
     # Submitting the sign in form
+    # TODO case insensitive email look ups
     def create
         # Require email and password params
+        if params[:email].blank? || params[:password].blank?
+            flash[:alert] = "Email and password are required"
+            render :new
+            return
+        end
+
         # Ensure that a user exists for that email
-        #  (case insensitive)
-        # If user is found
-        #   Match the password
-        #   If password matches
-        #     session[:current_user_id] = user.id
-        #   If not
-        #     render new with error
-        # If not
-        #   render new with error
+        user = User.find_by email: params[:email]
+        
+        if user.present?
+            session[:current_user_id] = user.id
+            redirect_to root_path, notice: "Welcome back, #{user.first_name}!"
+            #   Match the password
+            #   If password matches
+            #     session[:current_user_id] = user.id
+            #   If not
+            #     render new with error
+        else
+            flash[:alert] = "Email/password not found"
+            render :new
+        end
     end
     
     # Logging out
