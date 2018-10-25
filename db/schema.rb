d594206f2b0d8b73a247dfd7324e7a6c5ee472d5
Avand Amiri <avand@avandmamiri.com>
2018-10-25 02:55:51 +0000

    Adding password_digest to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181025022927) do
+ActiveRecord::Schema.define(version: 20181025025505) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20181025022927) do
     t.string  "last_name"
     t.integer "age"
     t.string  "email"
+    t.string  "password_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
